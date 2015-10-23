fd4cf8b28d7d03860caa3cce567abcd628c274ef
Jarbit Lira <jarbitlira@gmail.com>
2015-10-23 16:09:42 -0600

    add memcached

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ var request = require('request');
 var striptags = require('striptags');
 var S = require('string');
 
+var util = require('util');
+var memjs = require('memjs');
+var memcached = memjs.Client.create();
+var memcachedExpirationTime = (process.env.expirationTime || 60); // Default 1 min
+
 var server = app.listen(process.env.PORT || 3000);
 
 var url = 'http://www.cse.gob.ni/components/buscarcv.php';
@@ -21,21 +26,37 @@ app.get('/cedula/:cedula', function (req, res) {
     var params = req.params;
     var result = {};
 
-    request.post(url, {form: {'tipo': 'D', cedula: params.cedula}}, function (error, response, body) {
-        try {
-            if (response.statusCode == 200) {
-                body = S(striptags(body)).trim(); //remove html tags
-                result.nombre = (regexName.exec(body)[1]).split(':')[1].trim();
-                result.cedula = (regexCedula.exec(body)[1]).split(':')[1].trim();
-                result.departamento = (regexDepartamento.exec(body)[1]).split(':')[1].trim();
-                result.municipio = (regexMunicipio.exec(body)[1]).split(':')[1].trim();
-                result.direccion = (regexDireccion.exec(body)[1]).split(':')[1].trim();
-                res.json({data: result});
-            }
-        } catch (e) {
-            res.status(505).json({error: 'Documento no encontrado'});
-        }
+    var memcachedKey = util.format('cedula_%d', params.cedula);
+    memcached.get(memcachedKey, function (err, value, key) {
+        // value is not cached
+        if (value == null) {
+            request.post(url, {form: {'tipo': 'D', cedula: params.cedula}}, function (error, response, body) {
+                try {
+                    if (response.statusCode == 200) {
+                        body = S(striptags(body)).trim(); //remove html tags
+                        result.nombre = (regexName.exec(body)[1]).split(':')[1].trim();
+                        result.cedula = (regexCedula.exec(body)[1]).split(':')[1].trim();
+                        result.departamento = (regexDepartamento.exec(body)[1]).split(':')[1].trim();
+                        result.municipio = (regexMunicipio.exec(body)[1]).split(':')[1].trim();
+                        result.direccion = (regexDireccion.exec(body)[1]).split(':')[1].trim();
 
+                        // save result on memcached
+                        memcached.set(memcachedKey, JSON.stringify(result), function (err, success) {
+                            result.source = 'request';
+                            res.json({data: result});
+                        }, memcachedExpirationTime);
+
+                    }
+                } catch (e) {
+                    res.status(404).json({error: 'Documento no encontrado'});
+                }
+
+            });
+        } else {
+            var memcachedResponse = JSON.parse(value);
+            memcachedResponse.source = 'memcached';
+            res.json(memcachedResponse);
+        }
     });
 });
 
